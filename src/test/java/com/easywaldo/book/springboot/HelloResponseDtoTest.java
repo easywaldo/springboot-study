f0325dbd09eaf15a6406ff740ae65abfbc401279
easywaldoji <easywaldoji@gmail.com>
2020-01-02 23:33:57 +0900

    HelloResponseDto Lombok 적용
    Lombok 의존성 문제를 해결하기 위해 annotationProcessor 를 Gradle 에 설정한다.

---
+++ src/test/java/com/easywaldo/book/springboot/HelloResponseDtoTest.java
@@ -0,0 +1,24 @@
+package com.easywaldo.book.springboot;
+
+import com.easywaldo.book.springboot.web.HelloResponseDto;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void sut_should_operates_correctly() {
+
+        // Arrange
+        String expectedName = "test";
+        int expectedAmount = 1000;
+
+        // Act
+        HelloResponseDto dto = new HelloResponseDto(expectedName, expectedAmount);
+
+        // Assert
+        assertThat(dto.getName()).isEqualTo(expectedName);
+        assertThat(dto.getAmount()).isEqualTo(expectedAmount);
+    }
+}
