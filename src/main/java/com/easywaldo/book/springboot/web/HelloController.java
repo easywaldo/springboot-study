b7a3d44bec03a1cff2014b88741723a17c4182f1
easywaldoji <easywaldoji@gmail.com>
2020-05-28 01:17:29 +0900

    Multipart Files

--- src/main/java/com/easywaldo/book/springboot/web/HelloController.java
+++ src/main/java/com/easywaldo/book/springboot/web/HelloController.java
@@ -1,9 +1,13 @@
 package com.easywaldo.book.springboot.web;
 
 import com.easywaldo.book.springboot.web.dto.HelloResponseDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class HelloController {
@@ -16,6 +20,22 @@ public class HelloController {
     public HelloResponseDto helloDto(@RequestParam("name") String name,
                                      @RequestParam("amount") int amount) {
         return new HelloResponseDto(name, amount);
+    }
+
+    //@RequestMapping(value = "/hello/file", method = RequestMethod.POST)
+    @PostMapping("/hello/file")
+    @ResponseStatus(HttpStatus.CREATED)
+    public List<String> upload(@RequestParam("file") List<MultipartFile> files) throws Exception {
+        List<String> list = new ArrayList<>();
+        for (MultipartFile file : files) {
+            String originalfileName = file.getOriginalFilename();
+            System.out.print(originalfileName);
+            //File dest = new File("/Users/leejinam/Downloads/" + originalfileName);
+            //file.transferTo(dest);
+            // TODO
+            list.add(originalfileName);
+        }
+        return list;
     }
 }
 
